a4e365b366f75faf102ebb88ee4193cb1b06e64b
Leonardo Dutra <leodutranew@gmail.com>
2018-12-07 01:13:51 -0200

    #11 - Fornecedor CRUD e outros associados

--- implementacao/MercadoAutomobilistico/src/br/ufc/mercadoauto/App.java
+++ implementacao/MercadoAutomobilistico/src/br/ufc/mercadoauto/App.java
@@ -1,13 +1,23 @@
 package br.ufc.mercadoauto;
 
 import br.ufc.mercadoauto.model.cliente.Cliente;
+import br.ufc.mercadoauto.model.fornecedor.Fornecedor;
 import br.ufc.mercadoauto.repositorios.ClienteRepositorio;
+import br.ufc.mercadoauto.repositorios.FornecedorRepositorio;
 
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		
+		//clienteTeste();
+		fornecedorTeste();
+
+		
+	}
+	
+	
+	public static void clienteTeste() {
 		ClienteRepositorio clientes = new ClienteRepositorio();
 		
 		// Add Cliente
@@ -43,7 +53,45 @@ public class App {
 			System.out.println("Cod ==> " + cs.getCod() + " Nome ==> " + cs.getNome());
 			System.out.println(" -------------------------------------------- ");
 		}
+	}
+	
+	
+	public static void fornecedorTeste() {
+		FornecedorRepositorio fornecedores = new FornecedorRepositorio();
 		
+		// Add Fornecedor
+		Fornecedor leonardo = new Fornecedor(5, "Marcos Roberto", "Rua Figueiredo, 550", "(12) 9.5010-1010", "Cuba", "América Central");
+		Fornecedor maria = new Fornecedor(100, "Ivan Marques", "Rua Brasil, 122", "(61) 9.0180-2310", "Brasil", "América");
+		
+		fornecedores.inserir(leonardo);
+		fornecedores.inserir(maria);
+		
+		System.out.println("Total de registros => " + fornecedores.totalRegistros() + "\n");
+		
+		
+		for(Fornecedor cs : fornecedores.listar()) {
+			System.out.println(" -------------------------------------------- ");
+			System.out.println("Cod ==> " + cs.getCod() + " Nome ==> " + cs.getNome());
+			System.out.println(" -------------------------------------------- ");
+		}
+		
+		
+		// atualizando Fornecedor
+		leonardo.setNome("Fornecedor Novo");
+		Fornecedor fTemp = fornecedores.obter(5);
+		System.out.println("\nItem atualizado => " + fTemp.getNome());
+		
+		// deletando fornecedor
+		fornecedores.delete(maria);
+		
+		System.out.println("Total de registros Agora => " + fornecedores.totalRegistros() + "  //  Lista Vazia? == " + fornecedores.listaVazia() + " \n");
+		
+		
+		for(Fornecedor cs : fornecedores.listar()) {
+			System.out.println(" -------------------------------------------- ");
+			System.out.println("Cod ==> " + cs.getCod() + " Nome ==> " + cs.getNome());
+			System.out.println(" -------------------------------------------- ");
+		}
 	}
 	
 }
